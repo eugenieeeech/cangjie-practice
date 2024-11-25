692f4a8254deccb4ae10d1a301dd2ccd28bb9451
Eugenie Cheng <56400884+eugenieeeech@users.noreply.github.com>
2024-11-25 05:18:17 +0000

    feat: add dictionary as game database

---
+++ src/constant/dictionary.ts
@@ -0,0 +1,6 @@
+export const dictionary = [
+{ char: '一', code: 'm', meaning: '一 / one', hint: '一' },
+{ char: '我', code: 'hqi', meaning: '我 / I, me', hint: '竹手戈' },
+{ char: '你', code: 'oni', meaning: '你 / you', hint: '' },
+{ char: '好', code: 'vnd', meaning: '好 / good', hint: '' },
+];
