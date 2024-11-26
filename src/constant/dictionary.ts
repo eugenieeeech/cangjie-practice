1bc7d9ca39a38adc245f61c8a7438a892b68f355
Eugenie Cheng <56400884+eugenieeeech@users.noreply.github.com>
2024-11-26 08:14:30 +0000

    feat: update dictionary with new characters and hints

--- src/constant/dictionary.ts
+++ src/constant/dictionary.ts
@@ -1,6 +1,6 @@
 export const dictionary = [
 { char: '一', code: 'm', meaning: '一 / one', hint: '一' },
+{ char: '二', code: 'mm', meaning: '二 / two', hint: '一一' },
 { char: '我', code: 'hqi', meaning: '我 / I, me', hint: '竹手戈' },
-{ char: '你', code: 'oni', meaning: '你 / you', hint: '' },
-{ char: '好', code: 'vnd', meaning: '好 / good', hint: '' },
+{ char: '好', code: 'vnd', meaning: '好 / good', hint: '女弓木' },
 ];
