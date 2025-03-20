c2d9f3c6f01243a953f6f57cae0d64830b30a5d0
Eugenie Cheng <56400884+eugenieeeech@users.noreply.github.com>
2025-03-20 11:17:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cangjie-practice/'
+  base: '/cangjie-practice/',
   plugins: [react()],
 })
